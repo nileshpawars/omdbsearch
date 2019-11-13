7efdefebf9a4dc7567725bb1c0f78fb23e22a422
nileshpawars <57569922+nileshpawars@users.noreply.github.com>
2019-11-14 03:25:31 +0530

    update files

---
+++ precache-manifest.e0c79509bd7eea8038fdc985ef80ba38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5bff22d19273636d3effaed6867d52",
+    "url": "/omdbsearch/index.html"
+  },
+  {
+    "revision": "79073dc024ddbbd18550",
+    "url": "/omdbsearch/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b4944428d00a02a1e77e",
+    "url": "/omdbsearch/static/css/main.587037c0.chunk.css"
+  },
+  {
+    "revision": "79073dc024ddbbd18550",
+    "url": "/omdbsearch/static/js/2.4d086b52.chunk.js"
+  },
+  {
+    "revision": "b4944428d00a02a1e77e",
+    "url": "/omdbsearch/static/js/main.928fdb11.chunk.js"
+  },
+  {
+    "revision": "303980eba82a19d94d49",
+    "url": "/omdbsearch/static/js/runtime-main.c145c9db.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.644411757489111cdb99cd975208a821.js"
+  "/omdbsearch/precache-manifest.e0c79509bd7eea8038fdc985ef80ba38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/omdbsearch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
