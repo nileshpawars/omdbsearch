0976fadd4dabbe13c27cbc9b80191c8d785c7ae7
nileshpawars <57569922+nileshpawars@users.noreply.github.com>
2019-11-14 03:20:07 +0530

    new files added

---
+++ precache-manifest.644411757489111cdb99cd975208a821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5263bafff3bc30b21351db1490115eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79073dc024ddbbd18550",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f19f485952c14f599b14",
+    "url": "./static/css/main.587037c0.chunk.css"
+  },
+  {
+    "revision": "79073dc024ddbbd18550",
+    "url": "./static/js/2.4d086b52.chunk.js"
+  },
+  {
+    "revision": "f19f485952c14f599b14",
+    "url": "./static/js/main.f4bcd882.chunk.js"
+  },
+  {
+    "revision": "eff11bc8463326c84df6",
+    "url": "./static/js/runtime-main.fcc41483.js"
+  }
+]);
